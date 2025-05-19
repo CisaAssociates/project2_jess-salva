841b7634cb6026fb884a7da21fa7b743a3f28885
Joshua <albolersajohualuis@gmail.com>
2025-05-19 23:25:38 +0800

    fasf

--- table.php
+++ table.php
@@ -7,6 +7,7 @@ try{
     $sql = "DELETE FROM nodes WHERE node_id = :node_id ";
     $stmt = $conn->prepare($sql);
     $stmt->bindParam(":node_id",$node_id,PDO::PARAM_INT);
+    $stmt->execute();
 
 }catch(PDOException $e){
     echo "Message".$e->getMessage();
