8ad7a4ea39081c0b5146d21e5f64be475bd382be
Joshua <albolersajohualuis@gmail.com>
2025-05-19 23:23:41 +0800

    sad

--- table.php
+++ table.php
@@ -3,7 +3,10 @@
 include './config.php';
 
 try{
-    
+    $node_id = 101;
+    $sql = "DELETE FROM nodes WHERE node_id = :node_id ";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(":node_id",$node_id,PDO::PARAM_INT);
 
 }catch(PDOException $e){
     echo "Message".$e->getMessage();
