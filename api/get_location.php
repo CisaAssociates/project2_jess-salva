b9e0328afbb43556c17c54f197463657c7b706d5
Joshua <albolersajohualuis@gmail.com>
2025-05-20 15:24:31 +0800

    fsdafa

--- api/get_location.php
+++ api/get_location.php
@@ -3,6 +3,12 @@
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 
+// Add these headers to prevent caching on the client side and by proxies
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // A date in the past
+
 include "../config.php";
 
 try {
