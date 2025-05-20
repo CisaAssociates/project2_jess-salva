34cb73e1dcdc2d39f508894e67aac5f391e00907
Joshua <albolersajohualuis@gmail.com>
2025-05-20 15:29:42 +0800

    fsdaf

--- table.php
+++ table.php
@@ -2,14 +2,7 @@
 
 include "./config.php";
 
-$sql = "SELECT n.*
-FROM nodes n
-JOIN (
-    SELECT node_id, MAX(datetime) AS latest_time
-    FROM nodes
-    GROUP BY node_id
-) latest
-ON n.node_id = latest.node_id AND n.datetime = latest.latest_time;";
+$sql = "SELECT * FROM nodes ";
 $stmt = $conn->prepare($sql);
 $stmt->execute();
 $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
